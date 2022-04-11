6adc89eb1dd6ce0f91cd6cc90ba3fda98a4b90cf
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2022-04-11 08:49:47 -0400

    fix: svgr

--- next.config.js
+++ next.config.js
@@ -1,21 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack(config) {
+  webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: { and: [/\.(js|ts|md)x?$/] },
-      use: [
-        {
-          loader: "@svgr/webpack",
-          options: {
-            prettier: false,
-            svgo: true,
-            svgoConfig: { plugins: [{ removeViewBox: false }] },
-            titleProp: true,
-          },
-        },
-      ],
+      use: ["@svgr/webpack"],
     });
     return config;
   },
