f5b24155ceed148ea99657b975779515e270bac0
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2022-04-10 23:40:47 -0400

    feat: style footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,7 +1,29 @@
+import { IoLogoLinkedin, IoLogoGithub } from "react-icons/io5";
 import React, { FC } from "react";
 
 const Footer: FC = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="flex flex-col align-center space-y-4 p-16 bg-green-500 border-t-1 border-green-300 text-gray-900">
+      <div className="flex justify-center flex-row space-x-4">
+        <a
+          href="https://github.com/fluffybucketsnake"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <IoLogoGithub size={32} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/fluffybucketsnake-1379/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <IoLogoLinkedin size={32} />
+        </a>
+      </div>
+      <span className="text-center text-lg">Developed by me :)</span>
+      <span className="text-center text-sm">Made with Next.js</span>
+    </footer>
+  );
 };
 
 export default Footer;
