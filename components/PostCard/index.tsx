7476bdea87e0fba86012b9b54ea13fa5d98b3574
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2022-04-11 00:45:23 -0400

    feat: style post cards

--- components/PostCard/index.tsx
+++ components/PostCard/index.tsx
@@ -1,16 +1,23 @@
 import { PostMetadata } from "lib/types/PostMetadata";
+import Link from "next/link";
 import React, { FC } from "react";
 
 export type PostCardProps = {
   post: PostMetadata;
 };
 
-const PostCard: FC<PostCardProps> = ({ post: { title, description } }) => {
+const PostCard: FC<PostCardProps> = ({
+  post: { slug, title, description },
+}) => {
   return (
-    <article>
-      <h4>{title}</h4>
-      <p>{description}</p>
-    </article>
+    <Link href={`/blog/posts/${slug}`} passHref>
+      <a className="block">
+        <article className="flex flex-col p-4 bg-gray-800 border-1 border-gray-700 rounded-lg shadow">
+          <h4 className="text-3xl mb-2">{title}</h4>
+          <p className="text-lg text-gray-300">{description}</p>
+        </article>
+      </a>
+    </Link>
   );
 };
 
--- components/RecentPosts/index.tsx
+++ components/RecentPosts/index.tsx
@@ -1,5 +1,8 @@
+import ChevronIcon from "@fluentui/svg-icons/icons/chevron_right_24_regular.svg";
 import PostCard from "components/PostCard";
 import { PostMetadata } from "lib/types/PostMetadata";
+import Image from "next/image";
+import Link from "next/link";
 import React, { FC } from "react";
 
 export type RecentPostsProps = {
@@ -8,13 +11,19 @@ export type RecentPostsProps = {
 
 const RecentPosts: FC<RecentPostsProps> = ({ posts }) => {
   return (
-    <section>
-      <h3>Recent posts</h3>
-      <ol>
+    <section className="flex flex-col">
+      <h3 className="text-5xl mb-8">Recent posts</h3>
+      <ol className="list space-y-4 mb-8">
         {posts.map((post) => (
           <PostCard key={post.slug} post={post} />
         ))}
       </ol>
+      <Link href="/blog" passHref>
+        <a className="flex flex-row text-base items-center uppercase p-2 self-end">
+          See more
+          <ChevronIcon className="fill-white ml-1" />
+        </a>
+      </Link>
     </section>
   );
 };
