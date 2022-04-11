a25ad21a12be7b3c79612164d79d3e95ca43495d
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2022-04-10 23:17:53 -0400

    feat: add styling to the hero component

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -2,14 +2,17 @@ import React, { FC } from "react";
 
 const Hero: FC = () => {
   return (
-    <header>
-      <h1>
-        Hi, I'm <strong>Bruno Henrique Glowaski Morais</strong>
+    <header className="flex flex-col justify-center align-center p-32 bg-blue-600 border-b-1 border-blue-400 text-white text-center">
+      <h1 className="text-6xl mb-4 break-all">
+        Hi, I'm
+        <strong className="text-green-300 before:content-['\A'] before:whitespace-pre">
+          Bruno Henrique Glowaski Morais
+        </strong>
       </h1>
-      <h2>
-        AKA <strong>FluffyBucketSnake</strong>
+      <h2 className="text-xl font-bold mb-8">
+        AKA <strong className="text-green-300">FluffyBucketSnake</strong>
       </h2>
-      <p>
+      <p className="text-lg">
         I'm full-stack web developer and a game designer. I'm also passionate
         about gaming, science, technology and art.
       </p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: { borderWidth: { 1: "1px" } },
   },
   plugins: [],
 };
