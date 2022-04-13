29a3297cc9bfa3abcb3cdf6ad374433d0dfafdbb
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2022-04-13 00:47:33 -0400

    refactor: create DefaultLayout

---
+++ layouts/DefaultLayout/index.tsx
@@ -0,0 +1,18 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import NavMenu from "components/NavMenu";
+import { FC, useState } from "react";
+
+const DefaultLayout: FC = ({ children }) => {
+  const [navMenuOpen, setNavMenuOpen] = useState<boolean>(false);
+  return (
+    <>
+      <NavBar onOpenNavMenu={() => setNavMenuOpen(true)} />
+      <NavMenu open={navMenuOpen} onClose={() => setNavMenuOpen(false)} />
+      <main className="mt-[76px] p-16 pt-0">{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default DefaultLayout;
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,28 +1,18 @@
-import Footer from "components/Footer";
-import Hero from "components/Hero";
-import NavBar from "components/NavBar";
-import NavMenu from "components/NavMenu";
 import RecentPosts from "components/RecentPosts";
+import DefaultLayout from "layouts/DefaultLayout";
 import { PostMetadata } from "lib/types/PostMetadata";
 import { getAllPosts } from "lib/utils/getAllPosts";
 import type { GetStaticProps, NextPage } from "next";
-import { useState } from "react";
 
 type Props = {
   posts: PostMetadata[];
 };
 
 const Home: NextPage<Props> = ({ posts }) => {
-  const [navMenuOpen, setNavMenuOpen] = useState<boolean>(false);
   return (
-    <>
-      <NavBar onOpenNavMenu={() => setNavMenuOpen(true)} />
-      <NavMenu open={navMenuOpen} onClose={() => setNavMenuOpen(false)} />
-      <main className="p-16">
-        <RecentPosts posts={posts} />
-      </main>
-      <Footer />
-    </>
+    <DefaultLayout>
+      <RecentPosts posts={posts} />
+    </DefaultLayout>
   );
 };
 
