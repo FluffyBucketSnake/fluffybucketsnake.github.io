5369f663256c7eece7f213199afce9f1044091c4
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2022-04-16 00:56:51 -0400

    fix: change post card style

--- components/PostCard/index.tsx
+++ components/PostCard/index.tsx
@@ -12,8 +12,10 @@ const PostCard: FC<PostCardProps> = ({
   return (
     <Link href={`/blog/posts/${slug}`} passHref>
       <a className="group block">
-        <article className="flex flex-col p-4 bg-gray-800 border-1 group-hover:bg-gray-700 group-active:bg-gray-900 border-gray-700 group-hover:border-gray-600 group-active:border-gray-800 rounded-lg shadow">
-          <h4 className="text-3xl mb-2">{title}</h4>
+        <article className="flex flex-col p-4 bg-gray-800 border-1 border-gray-700 rounded-lg shadow">
+          <h4 className="text-3xl mb-2 group-hover:underline group-hover:text-blue-500 group-active:text-blue-600">
+            {title}
+          </h4>
           <p className="text-lg text-gray-300">{description}</p>
         </article>
       </a>
