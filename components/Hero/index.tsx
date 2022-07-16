545952dea6bcd29dc5bb6a82204e68a55807b749
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2022-07-16 19:41:55 -0400

    fix: strong hero text color too bright

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -5,12 +5,12 @@ const Hero: FC = () => {
     <header className="flex flex-col justify-center align-center px-16 py-32 bg-hero-main border-b-1 border-primary-400 text-primary-contrast text-center overflow-hidden">
       <h1 className="text-6xl text-center relative">
         Hi, I'm
-        <strong className="font-display text-secondary-400 text-center before:content-['\A'] before:whitespace-pre">
+        <strong className="font-display text-secondary-500 text-center before:content-['\A'] before:whitespace-pre">
           Bruno Henrique Glowaski Morais
         </strong>
       </h1>
       <h2 className="text-xl font-bold mb-8">
-        AKA <strong className="text-secondary-400">FluffyBucketSnake</strong>
+        AKA <strong className="text-secondary-500">FluffyBucketSnake</strong>
       </h2>
       <p className="text-lg">
         I'm full-stack web developer and a game designer. I'm also passionate
