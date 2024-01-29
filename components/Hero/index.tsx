6e234fcd449017ded54cc8a4b18956a5ce6feefc
FluffyBucketSnake <fluffybucketsnake@outlook.com>
2024-01-29 03:53:24 -0400

    fix: basic error display

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,27 +1,39 @@
 import { wavesEffect } from "lib/effects/wavesEffect";
-import React, { FC, useEffect, useRef } from "react";
+import React, { FC, useEffect, useRef, useState } from "react";
 
 const Hero: FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [error, setError] = useState<unknown>();
 
-  useEffect(wavesEffect(canvasRef), [canvasRef.current]);
+  useEffect(wavesEffect(canvasRef, setError), [canvasRef.current]);
+  useEffect(() => {
+    if (error == null) return;
+    console.error(error);
+  }, [error]);
 
   return (
     <header className="relative flex flex-col justify-center align-center px-16 py-32 bg-hero-main bg-cover bg-center border-b-1 border-primary-400 text-primary-contrast text-center overflow-hidden">
-      <canvas className="absolute inset-0 w-full h-full -z-1" ref={canvasRef} />
-      <h1 className="text-6xl text-center relative">
+      {error == null && (
+        <canvas className="absolute inset-0 w-full h-full" ref={canvasRef} />
+      )}
+      <h1 className="text-6xl text-center relative z-10">
         Hi, I'm
         <strong className="font-display text-secondary-500 text-center before:content-['\A'] before:whitespace-pre">
           Bruno Henrique Glowaski Morais
         </strong>
       </h1>
-      <h2 className="text-xl font-bold mb-8">
+      <h2 className="text-xl font-bold mb-8 z-10">
         AKA <strong className="text-secondary-500">FluffyBucketSnake</strong>
       </h2>
-      <p className="text-lg">
+      <p className="text-lg z-10">
         I'm full-stack web developer and a game designer. I'm also passionate
         about gaming, science, technology and art.
       </p>
+      {error != null && (
+        <span className="absolute right-4 bottom-4 px-2 py-1 text-error bg-bg-default/50 rounded z-20">
+          An error occurred while rendering the banner: "{`${error}`}"
+        </span>
+      )}
     </header>
   );
 };
--- lib/effects/wavesEffect.ts
+++ lib/effects/wavesEffect.ts
@@ -3,13 +3,23 @@ import { WebGL2Renderer } from "lib/renderers/webgl2";
 import { SimpleLoop } from "lib/utils/interactive";
 import { RefObject } from "react";
 
-export function wavesEffect(canvas: RefObject<HTMLCanvasElement>) {
+export function wavesEffect(
+  canvas: RefObject<HTMLCanvasElement>,
+  setError: (error: unknown) => void,
+) {
   return () => {
-    const renderer = new WebGL2Renderer(canvas.current!, FRAG_SRC);
-    const loop = new SimpleLoop((time) => renderer.renderFrame(time));
-    return () => {
-      loop.stop();
-      renderer.dispose();
-    };
+    if (canvas.current == null) {
+      return;
+    }
+    try {
+      const renderer = new WebGL2Renderer(canvas.current!, FRAG_SRC);
+      const loop = new SimpleLoop((time) => renderer.renderFrame(time));
+      return () => {
+        loop.stop();
+        renderer.dispose();
+      };
+    } catch (err) {
+      setError(err);
+    }
   };
 }
