5a3f202ccfb458abb6b26b0925959dcd9f09e1aa
Bruno Henrique 'FluffyBucketSnake' Glowaski Morais <fluffybucketsnake@outlook.com>
2024-05-28 12:53:26 -0400

    feat: storybook

---
+++ src/stories/atom-button.stories.ts
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/svelte';
+
+import AtomButton from '$lib/atoms/atom-button.svelte';
+
+const meta: Meta<typeof AtomButton> = {
+	title: 'Atoms/Button',
+	component: AtomButton,
+	args: {
+		children: 'Button',
+		color: 'Primary'
+	},
+	argTypes: {
+		children: {
+			name: 'Text',
+			control: 'text'
+		},
+		color: {
+			control: 'select',
+			options: ['Primary', 'Secondary'],
+			mapping: {
+				Primary: 'primary',
+				Secondary: 'secondary'
+			}
+		}
+	}
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Button: Story = {
+	args: {}
+};
