e1ee9f1e3ee0ba1045868c2182c992dba04963cc
Bruno Henrique 'FluffyBucketSnake' Glowaski Morais <fluffybucketsnake@outlook.com>
2024-06-02 16:34:02 -0400

    feat: button text variant

--- src/lib/atoms/atom-button.svelte
+++ src/lib/atoms/atom-button.svelte
@@ -4,6 +4,7 @@
 	export interface ISharedProps {
 		color?: 'primary' | 'secondary';
 		shadow?: boolean | '8px';
+		variant?: 'filled' | 'text';
 	}
 
 	export interface ILinkVariantProps extends HTMLAnchorAttributes {
@@ -16,25 +17,48 @@
 
 	export type Props = ISharedProps & (ILinkVariantProps | IActionVariantProps);
 
-	const baseClasses =
-		'border p-2 font-stylized align-middle text-center transition duration-75 ease-in-out focus:ring-0';
-	const primaryClasses =
-		'border-primary-400 hover:border-primary-200 focus:border-primary-200 active:border-primary-500 bg-primary-500 hover:bg-primary-400 focus:bg-primary-400 active:bg-primary-600 text-primary-fg';
-	const secondaryClasses =
-		'border-secondary-400 hover:border-secondary-200 active:border-secondary-500 bg-secondary-500 hover:bg-secondary-400 active:bg-secondary-600 text-secondary-fg';
-	const shadow4pxClasses = 'shadow-4px hover:shadow-8px focus:shadow-8px active:shadow-2px';
-	const shadow8pxClasses = 'shadow-8px hover:shadow-16px focus:shadow-16px active:shadow-4px';
+	const style = {
+		base: 'font-stylized align-middle text-center transition duration-75 ease-in-out focus:outline-0',
+		variants: {
+			filled: {
+				base: 'border p-2',
+				primary:
+					'border-primary-400 hover:border-primary-200 focus:border-primary-200 active:border-primary-500 bg-primary-500 hover:bg-primary-400 focus:bg-primary-400 active:bg-primary-600 text-primary-fg',
+				secondary:
+					'border-secondary-400 hover:border-secondary-200 active:border-secondary-500 bg-secondary-500 hover:bg-secondary-400 active:bg-secondary-600 text-secondary-fg'
+			},
+			text: {
+				base: 'p-1 border-transparent bg-transparent hover:bg-fg/10 focus:bg-fg/10',
+				primary: '',
+				secondary: ''
+			}
+		},
+		shadows: {
+			box: {
+				4: 'shadow-4px hover:shadow-8px focus:shadow-8px active:shadow-2px',
+				8: 'shadow-8px hover:shadow-16px focus:shadow-16px active:shadow-4px'
+			},
+			drop: {
+				4: 'drop-shadow-4px hover:drop-shadow-8px focus:drop-shadow-8px active:drop-shadow-2px',
+				8: 'drop-shadow-8px hover:drop-shadow-16px focus:drop-shadow-16px active:drop-shadow-4px'
+			}
+		}
+	};
 </script>
 
 <script lang="ts">
-	const { href, children, color = 'primary', shadow = false, ...attrs }: Props = $props();
+	const {
+		href,
+		children,
+		color = 'primary',
+		shadow = false,
+		variant = 'filled',
+		...attrs
+	}: Props = $props();
 
-	const isPrimary = $derived(color == 'primary');
-	const isSecondary = $derived(color == 'secondary');
-	const uses4pxShadow = $derived(shadow === true);
-	const uses8pxShadow = $derived(shadow === '8px');
+	const useDropShadow = $derived(variant == 'text');
 	const classes = $derived(
-		`${baseClasses} ${isPrimary && primaryClasses} ${isSecondary && secondaryClasses} ${uses4pxShadow && shadow4pxClasses} ${uses8pxShadow && shadow8pxClasses} ${attrs.class}`
+		`${style.base} ${style.variants[variant].base} ${style.variants[variant][color]} ${shadow && style.shadows[useDropShadow ? 'drop' : 'box'][shadow === '8px' ? 8 : 4]} ${attrs.class}`
 	);
 </script>
 
