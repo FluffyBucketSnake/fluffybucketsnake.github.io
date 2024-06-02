db8342513834273eec8b359deb75be875c0f3ab0
Bruno Henrique 'FluffyBucketSnake' Glowaski Morais <fluffybucketsnake@outlook.com>
2024-06-02 01:48:13 -0400

    feat: button animation

--- src/lib/atoms/atom-button.svelte
+++ src/lib/atoms/atom-button.svelte
@@ -16,13 +16,14 @@
 
 	export type Props = ISharedProps & (ILinkVariantProps | IActionVariantProps);
 
-	const baseClasses = 'border p-2 font-stylized align-middle text-center';
+	const baseClasses =
+		'border p-2 font-stylized align-middle text-center transition duration-75 ease-in-out focus:ring-0';
 	const primaryClasses =
-		'border-primary-400 hover:border-primary-200 active:border-primary-500 bg-primary-500 hover:bg-primary-400 active:bg-primary-600 text-primary-fg';
+		'border-primary-400 hover:border-primary-200 focus:border-primary-200 active:border-primary-500 bg-primary-500 hover:bg-primary-400 focus:bg-primary-400 active:bg-primary-600 text-primary-fg';
 	const secondaryClasses =
 		'border-secondary-400 hover:border-secondary-200 active:border-secondary-500 bg-secondary-500 hover:bg-secondary-400 active:bg-secondary-600 text-secondary-fg';
-	const shadow4pxClasses = 'shadow-4px hover:shadow-8px active:shadow-2px';
-	const shadow8pxClasses = 'shadow-8px hover:shadow-16px active:shadow-4px';
+	const shadow4pxClasses = 'shadow-4px hover:shadow-8px focus:shadow-8px active:shadow-2px';
+	const shadow8pxClasses = 'shadow-8px hover:shadow-16px focus:shadow-16px active:shadow-4px';
 </script>
 
 <script lang="ts">
