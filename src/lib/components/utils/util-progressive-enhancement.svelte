e6e8e5423478b56c4b2b996bfd255596ebf29c36
Bruno Henrique 'FluffyBucketSnake' Glowaski Morais <fluffybucketsnake@outlook.com>
2024-06-07 23:47:29 -0400

    feat: progressive enhancement util

---
+++ src/lib/components/utils/util-progressive-enhancement.svelte
@@ -0,0 +1,24 @@
+<script lang="ts" context="module">
+	import type { Snippet } from 'svelte';
+
+	export interface Props {
+		fallback?: Snippet;
+		children: Snippet;
+	}
+</script>
+
+<script lang="ts">
+	let { fallback, children }: Props = $props();
+
+	let hasJs = $state(false);
+
+	$effect(() => {
+		hasJs = true;
+	});
+</script>
+
+{#if hasJs}
+	{@render children()}
+{:else if fallback != null}
+	{@render fallback()}
+{/if}
