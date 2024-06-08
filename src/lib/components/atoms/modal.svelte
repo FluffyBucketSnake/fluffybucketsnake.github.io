56893e24525a1971240fabd0e43647dcce9f66a6
Bruno Henrique 'FluffyBucketSnake' Glowaski Morais <fluffybucketsnake@outlook.com>
2024-06-07 23:45:53 -0400

    feat: animations and better ergonomics for modal

--- src/lib/components/atoms/modal.svelte
+++ src/lib/components/atoms/modal.svelte
@@ -1,26 +1,56 @@
 <script lang="ts" context="module">
 	import type { Snippet } from 'svelte';
-	import type { HTMLAttributes } from 'svelte/elements';
+	import type { EventHandler, HTMLAttributes } from 'svelte/elements';
 
-	interface Props extends HTMLAttributes<HTMLDialogElement> {
-		class?: HTMLAttributes<HTMLElement>['class'];
+	export interface ActivatorProps {
+		onclick: EventHandler<MouseEvent>;
+	}
+
+	export interface Props extends HTMLAttributes<HTMLDialogElement> {
+		open?: boolean;
+		activator?: Snippet<[ActivatorProps]>;
 		header?: Snippet;
 		children?: Snippet;
 	}
 </script>
 
 <script lang="ts">
-	let { class: classes, header, children, ...attrs }: Props = $props();
+	let {
+		open = $bindable(),
+		class: classes,
+		activator,
+		header,
+		children,
+		onclose,
+		...attrs
+	}: Props = $props();
 
 	let modalRef: HTMLDialogElement | undefined = $state();
 
-	export const show = () => modalRef?.showModal();
-	export const close = () => modalRef?.close();
+	$effect(() => {
+		if (open) {
+			modalRef?.showModal();
+		} else {
+			modalRef?.close();
+		}
+	});
+
+	const onClose: EventHandler<Event, HTMLDialogElement> = (event) => {
+		open = false;
+		onclose?.(event);
+	};
+
+	export const show = () => (open = true);
+	export const close = () => (open = false);
 </script>
 
+{#if activator != null}
+	{@render activator({ onclick: () => show() })}
+{/if}
 <dialog
-	class="backdrop:backdrop-blur-3xl backdrop:bg-darkest/50 {classes}"
+	class="backdrop:backdrop-blur-3xl backdrop:bg-darkest/50 backdrop:animate-fade-out open:backdrop:animate-fade-in max-w-[unset] max-h-[unset] text-fg {classes}"
 	bind:this={modalRef}
+	onclose={onClose}
 	{...attrs}
 >
 	{#if header}
