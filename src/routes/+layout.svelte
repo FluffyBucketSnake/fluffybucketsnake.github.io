72528bdcdce446dd40b5db6ac4a861ce882ecadc
Bruno Henrique 'FluffyBucketSnake' Glowaski Morais <fluffybucketsnake@outlook.com>
2024-07-25 02:30:56 -0400

    refactor: keep layout inside page files

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,4 @@
 	import '../app.css';
 </script>
 
-<div class="flex flex-col items-center overflow-x-hidden w-screen">
-	<slot></slot>
-</div>
+<slot></slot>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,182 +25,189 @@
 	let effectError: unknown = $state();
 </script>
 
-<ProgressiveEnhancement>
-	<LayoutSidebar>
-		{#snippet activator({ onclick })}
-			<AtomButton
-				aria-label={$t('sidebar.labels.open')}
-				variant="text"
-				shadow="8px"
-				class="fixed left-2 top-2 p-2 z-10"
-				{onclick}
+<div class="flex flex-col overflow-x-clip">
+	<ProgressiveEnhancement>
+		<LayoutSidebar>
+			{#snippet activator({ onclick })}
+				<AtomButton
+					aria-label={$t('sidebar.labels.open')}
+					variant="text"
+					shadow="8px"
+					class="fixed left-2 top-2 p-2 z-10"
+					{onclick}
+				>
+					<IconMenu />
+				</AtomButton>
+			{/snippet}
+		</LayoutSidebar>
+	</ProgressiveEnhancement>
+
+	<header class="w-screen min-h-screen h-[546px] flex justify-center items-center">
+		<h1 class="flex flex-col items-center">
+			<span class="font-stylized text-2xl leading-none drop-shadow-4px">
+				{$t('home.title.0')}
+			</span>
+			<span
+				class="relative font-header leading-snug text-5xl text-secondary-400 text-center drop-shadow-8px"
 			>
-				<IconMenu />
-			</AtomButton>
-		{/snippet}
-	</LayoutSidebar>
-</ProgressiveEnhancement>
+				<AnimatedText
+					text={$t('home.title.1')}
+					class="relative animate-wave"
+					style={(i) => `animation-delay: -${i * 110}ms`}
+				/>
+				<wbr />
+				<AnimatedText
+					text={$t('home.title.2')}
+					class="relative animate-wave"
+					style={(i) => `animation-delay: -${(i + 6) * 110}ms`}
+				/>
+				<wbr />
+				<AnimatedText
+					text={$t('home.title.3')}
+					class="relative animate-wave"
+					style={(i) => `animation-delay: -${(i + 12) * 110}ms`}
+				/>
+			</span>
+			<span class="mt-4 font-stylized text-2xl leading-none text-primary-200 drop-shadow-4px">
+				{$t('home.title.4')}
+			</span>
+		</h1>
+	</header>
 
-<header class="w-screen min-h-screen h-[546px] flex justify-center items-center">
-	<h1 class="flex flex-col items-center">
-		<span class="font-stylized text-2xl leading-none drop-shadow-4px">
-			{$t('home.title.0')}
-		</span>
-		<span
-			class="relative font-header leading-snug text-5xl text-secondary-400 text-center drop-shadow-8px"
+	{#snippet card(id: string, content: Snippet, href: string, side: 'left' | 'right')}
+		<div
+			use:appearOnScroll={side === 'left'
+				? { animation: 'opacity-1 translate-x-0', animationOut: 'opacity-0 -translate-x-full' }
+				: { animation: 'opacity-1 translate-x-0', animationOut: 'opacity-0 translate-x-full' }}
+			class="border border-carbon bg-acrylic/60 p-8 w-[304px] lg:w-[800px] flex flex-col gap-8 shadow-32px/A backdrop-blur-2xl ease-in-out transition duration-1000 translate-x-f"
 		>
-			<AnimatedText
-				text={$t('home.title.1')}
-				class="relative animate-wave"
-				style={(i) => `animation-delay: -${i * 110}ms`}
-			/>
-			<wbr />
-			<AnimatedText
-				text={$t('home.title.2')}
-				class="relative animate-wave"
-				style={(i) => `animation-delay: -${(i + 6) * 110}ms`}
-			/>
-			<wbr />
-			<AnimatedText
-				text={$t('home.title.3')}
-				class="relative animate-wave"
-				style={(i) => `animation-delay: -${(i + 12) * 110}ms`}
-			/>
-		</span>
-		<span class="mt-4 font-stylized text-2xl leading-none text-primary-200 drop-shadow-4px">
-			{$t('home.title.4')}
-		</span>
-	</h1>
-</header>
-
-{#snippet card(id: string, content: Snippet, href: string, side: 'left' | 'right')}
-	<div
-		use:appearOnScroll={side === 'left'
-			? { animation: 'opacity-1 translate-x-0', animationOut: 'opacity-0 -translate-x-full' }
-			: { animation: 'opacity-1 translate-x-0', animationOut: 'opacity-0 translate-x-full' }}
-		class="border border-carbon bg-acrylic/60 p-8 w-[304px] lg:w-[800px] flex flex-col gap-8 shadow-32px/A backdrop-blur-2xl ease-in-out transition duration-1000 translate-x-f"
+			<h2 class="font-header text-3xl text-primary-200 drop-shadow-4px">
+				{$t(`home.${id}.header`)}
+			</h2>
+			{@render content()}
+			<AtomButton class="self-stretch lg:self-end" color="primary" {href} shadow>
+				{$t(`home.${id}.cta`)}
+				{#snippet appendIcon()}
+					<IconChevronRight />
+				{/snippet}
+			</AtomButton>
+		</div>
+	{/snippet}
+	<section
+		class="mt-[50vh] w-screen min-h-screen h-[546px] flex justify-center lg:justify-start lg:pl-[112px] items-center"
 	>
-		<h2 class="font-header text-3xl text-primary-200 drop-shadow-4px">{$t(`home.${id}.header`)}</h2>
-		{@render content()}
-		<AtomButton class="self-stretch lg:self-end" color="primary" {href} shadow>
-			{$t(`home.${id}.cta`)}
+		{#snippet about()}
+			<main
+				class="prose prose-gray text-fg prose-strong:text-primary-200 prose-em:text-secondary-400 max-w-none"
+			>
+				<p>
+					{@html $t('home.about.text')}
+				</p>
+			</main>
+		{/snippet}
+		{@render card('about', about, '/about', 'left')}
+	</section>
+	<section
+		class="mt-[50vh] w-screen min-h-[200vh] h-[1092px] lg:min-h-[100vh] flex justify-center lg:justify-end lg:pr-[112px] items-center"
+	>
+		{#snippet blog()}
+			<ol class="lg:pb-2 flex flex-col lg:flex-row items-center gap-4 lg:overflow-x-scroll w-full">
+				{#each data.blogPosts as post (post.slug)}
+					<li>
+						<a href={post.link} class="group focus:outline-0">
+							<article
+								class="w-[240px] h-[160px] bg-matte shadow-4px group-hover:shadow-8px group-focus:shadow-8px group-active:shadow-2px flex flex-col p-2 gap-2 transition"
+							>
+								<h3
+									class="font-header text-lg leading-none group-hover:text-primary-200 group-focus:text-primary-200 group-active:text-primary-400"
+								>
+									{post.title}
+								</h3>
+								<span class="text-xs text-fg/75 leading-none">{formatPostDate(post.date)}</span>
+								<p class="text-sm flex-1 text-ellipsis overflow-hidden">{post.description}</p>
+							</article>
+						</a>
+					</li>
+				{/each}
+			</ol>
+		{/snippet}
+		{@render card('blog', blog, '/blog', 'right')}
+	</section>
+
+	<footer
+		class="mt-[50vh] w-screen min-h-screen h-[546px] p-16 flex flex-col justify-end items-center relative"
+	>
+		<AtomButton
+			href="/donate"
+			color="secondary"
+			shadow="8px"
+			class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+			use={[
+				[
+					appearOnScroll,
+					{ animation: 'animate-appear-down', animationOut: 'animate-disappear-down' }
+				]
+			]}
+		>
+			{$t('home.footer.donate')}
 			{#snippet appendIcon()}
 				<IconChevronRight />
 			{/snippet}
 		</AtomButton>
-	</div>
-{/snippet}
-<section
-	class="mt-[50vh] w-screen min-h-screen h-[546px] flex justify-center lg:justify-start lg:pl-[112px] items-center"
->
-	{#snippet about()}
-		<main
-			class="prose prose-gray text-fg prose-strong:text-primary-200 prose-em:text-secondary-400 max-w-none"
-		>
-			<p>
-				{@html $t('home.about.text')}
-			</p>
-		</main>
-	{/snippet}
-	{@render card('about', about, '/about', 'left')}
-</section>
-<section
-	class="mt-[50vh] w-screen min-h-[200vh] h-[1092px] lg:min-h-[100vh] flex justify-center lg:justify-end lg:pr-[112px] items-center"
->
-	{#snippet blog()}
-		<ol class="lg:pb-2 flex flex-col lg:flex-row items-center gap-4 lg:overflow-x-scroll w-full">
-			{#each data.blogPosts as post (post.slug)}
-				<li>
-					<a href={post.link} class="group focus:outline-0">
-						<article
-							class="w-[240px] h-[160px] bg-matte shadow-4px group-hover:shadow-8px group-focus:shadow-8px group-active:shadow-2px flex flex-col p-2 gap-2 transition"
-						>
-							<h3
-								class="font-header text-lg leading-none group-hover:text-primary-200 group-focus:text-primary-200 group-active:text-primary-400"
-							>
-								{post.title}
-							</h3>
-							<span class="text-xs text-fg/75 leading-none">{formatPostDate(post.date)}</span>
-							<p class="text-sm flex-1 text-ellipsis overflow-hidden">{post.description}</p>
-						</article>
-					</a>
-				</li>
-			{/each}
-		</ol>
-	{/snippet}
-	{@render card('blog', blog, '/blog', 'right')}
-</section>
-
-<footer
-	class="mt-[50vh] w-screen min-h-screen h-[546px] p-16 flex flex-col justify-end items-center relative"
->
-	<AtomButton
-		href="/donate"
-		color="secondary"
-		shadow="8px"
-		class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-		use={[
-			[appearOnScroll, { animation: 'animate-appear-down', animationOut: 'animate-disappear-down' }]
-		]}
-	>
-		{$t('home.footer.donate')}
-		{#snippet appendIcon()}
-			<IconChevronRight />
-		{/snippet}
-	</AtomButton>
-	<ul class="flex gap-4">
-		<li
-			class="transition ease-in-out duration-1000"
+		<ul class="flex gap-4">
+			<li
+				class="transition ease-in-out duration-1000"
+				use:appearOnScroll={{
+					animation: 'delay-100 opacity-1 translate-y-0',
+					animationOut: 'opacity-0 translate-y-full'
+				}}
+			>
+				<AtomButton
+					variant="text"
+					shadow="8px"
+					aria-label={$t('home.footer.socials.linkedin')}
+					href={me.links.linkedin}
+				>
+					<LogoLinkedIn />
+				</AtomButton>
+			</li>
+			<li
+				class="transition ease-in-out duration-1000"
+				use:appearOnScroll={{
+					animation: 'delay-200 opacity-1 translate-y-0',
+					animationOut: 'opacity-0 translate-y-full'
+				}}
+			>
+				<AtomButton
+					variant="text"
+					shadow="8px"
+					aria-label={$t('home.footer.socials.github')}
+					href={me.links.github}
+				>
+					<LogoGithub />
+				</AtomButton>
+			</li>
+		</ul>
+		<span
+			class="mt-8 font-stylized drop-shadow-4px transition ease-in-out duration-1000"
 			use:appearOnScroll={{
-				animation: 'delay-100 opacity-1 translate-y-0',
+				animation: 'delay-300 opacity-1 translate-y-0',
 				animationOut: 'opacity-0 translate-y-full'
 			}}
 		>
-			<AtomButton
-				variant="text"
-				shadow="8px"
-				aria-label={$t('home.footer.socials.linkedin')}
-				href={me.links.linkedin}
-			>
-				<LogoLinkedIn />
-			</AtomButton>
-		</li>
-		<li
-			class="transition ease-in-out duration-1000"
+			{$t('home.footer.author')}
+		</span>
+		<span
+			class="mt-1 font-stylized text-xs drop-shadow-4px transition ease-in-out duration-1000"
 			use:appearOnScroll={{
-				animation: 'delay-200 opacity-1 translate-y-0',
+				animation: 'delay-[400ms] opacity-1 translate-y-0',
 				animationOut: 'opacity-0 translate-y-full'
 			}}
 		>
-			<AtomButton
-				variant="text"
-				shadow="8px"
-				aria-label={$t('home.footer.socials.github')}
-				href={me.links.github}
-			>
-				<LogoGithub />
-			</AtomButton>
-		</li>
-	</ul>
-	<span
-		class="mt-8 font-stylized drop-shadow-4px transition ease-in-out duration-1000"
-		use:appearOnScroll={{
-			animation: 'delay-300 opacity-1 translate-y-0',
-			animationOut: 'opacity-0 translate-y-full'
-		}}
-	>
-		{$t('home.footer.author')}
-	</span>
-	<span
-		class="mt-1 font-stylized text-xs drop-shadow-4px transition ease-in-out duration-1000"
-		use:appearOnScroll={{
-			animation: 'delay-[400ms] opacity-1 translate-y-0',
-			animationOut: 'opacity-0 translate-y-full'
-		}}
-	>
-		{$t('home.footer.frameworks')}
-	</span>
-</footer>
+			{$t('home.footer.frameworks')}
+		</span>
+	</footer>
+</div>
 
 {#if effectError != null}
 	<DialogRenderError error={effectError}>
