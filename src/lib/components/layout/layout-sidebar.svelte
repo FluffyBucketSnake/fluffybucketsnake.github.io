0a59b8aa0484187bab876f5207d70f61a0a930ef
Bruno Henrique 'FluffyBucketSnake' Glowaski Morais <fluffybucketsnake@outlook.com>
2024-07-26 02:53:06 -0400

    feat: static mode for sidebar

--- src/lib/components/layout/layout-sidebar.svelte
+++ src/lib/components/layout/layout-sidebar.svelte
@@ -2,10 +2,20 @@
 	import type { ComponentType, Snippet } from 'svelte';
 	import Modal, { type ActivatorProps } from '$lib/components/base/modal.svelte';
 
-	export interface Props {
-		open?: boolean;
+	export interface ModalProps {
+		static?: false;
 		activator?: Snippet<[ActivatorProps]>;
 	}
+
+	export interface StaticProps {
+		static: true;
+	}
+
+	export interface SharedProps {
+		class?: string;
+	}
+
+	export type Props = SharedProps & (ModalProps | StaticProps);
 </script>
 
 <script lang="ts">
@@ -18,20 +28,16 @@
 	import { page } from '$app/stores';
 	import { t } from '$lib/i18n';
 
-	let { open = $bindable(), activator }: Props = $props();
+	let { ...props }: Props = $props();
 
 	let modalRef: Modal | undefined = $state();
-
-	export const show = $derived(modalRef!.show);
-	export const close = $derived(modalRef!.close);
 </script>
 
 {#snippet menuItem(icon: ComponentType, id: string, href: string, active: boolean)}
 	<a
 		{href}
-		class="
-			relative pl-5 pr-4 py-2 flex gap-2 items-center text-fg focus:outline-0
-  {active
+		class="relative pl-5 pr-4 py-2 flex gap-2 items-center text-fg focus:outline-0
+    {active
 			? "bg-fg/15 cursor-default before:absolute before:content-['_'] before:left-0 before:w-1 before:h-full before:bg-fg"
 			: 'hover:bg-fg/5 focus:bg-fg/5 active:bg-fg/15'}"
 	>
@@ -42,26 +48,37 @@
 	</a>
 {/snippet}
 
-<Modal
-	bind:this={modalRef}
-	bind:open
-	class="m-0 -left-8 -top-8 w-[calc(100%_+_32px)] lg:max-w-[272px] h-[calc(100%_+_32px)] lg:border-r border-carbon pl-8 pt-8 open:flex flex-col bg-acrylic/60 drop-shadow-32px open:animate-appear-left"
-	{activator}
->
-	<AtomButton
-		aria-label={$t('sidebar.labels.close')}
-		class="fixed top-10 left-10 p-2"
-		variant="text"
-		shadow
-		onclick={() => modalRef!.close()}
-	>
-		<IconClose />
-	</AtomButton>
+{#snippet menuItems()}
+	{@render menuItem(IconHouse, 'home', '/', $page.route.id === '/')}
+	{@render menuItem(IconNews, 'blog', '/blog', $page.route.id === '/blog')}
+	{@render menuItem(IconPerson, 'me', '/me', $page.route.id === '/me')}
+	{@render menuItem(IconCup, 'donate', '/donate', $page.route.id === '/donate')}
+{/snippet}
 
-	<nav class="flex-col mt-14">
-		{@render menuItem(IconHouse, 'home', '/', $page.route.id === '/')}
-		{@render menuItem(IconNews, 'blog', '/blog', $page.route.id === '/blog')}
-		{@render menuItem(IconPerson, 'me', '/me', $page.route.id === '/me')}
-		{@render menuItem(IconCup, 'donate', '/donate', $page.route.id === '/donate')}
+{#if props.static}
+	<nav
+		class="w-240px min-h-full py-4 flex flex-col bg-acrylic/60 open:animate-appear-left {props.class}"
+	>
+		{@render menuItems()}
 	</nav>
-</Modal>
+{:else}
+	<Modal
+		bind:this={modalRef}
+		class="m-0 -left-8 -top-8 w-[calc(100%_+_32px)] lg:max-w-[272px] h-[calc(100%_+_32px)] lg:border-r border-carbon pl-8 pt-8 open:flex flex-col bg-acrylic/60 drop-shadow-32px open:animate-appear-left {props.class}"
+		activator={props.activator}
+	>
+		<AtomButton
+			aria-label={$t('sidebar.labels.close')}
+			class="fixed top-10 left-10 p-2"
+			variant="text"
+			shadow
+			onclick={() => modalRef!.close()}
+		>
+			<IconClose />
+		</AtomButton>
+
+		<nav class="flex-col mt-14">
+			{@render menuItems()}
+		</nav>
+	</Modal>
+{/if}
