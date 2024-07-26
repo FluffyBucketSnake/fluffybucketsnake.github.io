9ce459c7f780721bd4a2794648afaa0cac17d263
Bruno Henrique 'FluffyBucketSnake' Glowaski Morais <fluffybucketsnake@outlook.com>
2024-07-26 02:35:00 -0400

    feat: footer layout component

---
+++ src/lib/components/layout/layout-footer.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import AtomButton from '$lib/components/atoms/atom-button.svelte';
+	import { t } from '$lib/i18n';
+	import LogoGithub from 'virtual:icons/pixelent/logo-github-32-filled';
+	import LogoLinkedIn from 'virtual:icons/pixelent/logo-linkedin-32-filled';
+	import me from '$lib/assets/me.json';
+</script>
+
+<footer
+	class="footer border-t border-secondary-400 p-16 bg-secondary-500 flex flex-col gap-2 items-center text-secondary-fg"
+>
+	<ul class="flex gap-4">
+		<li>
+			<AtomButton
+				variant="text"
+				color="inverted"
+				shadow
+				shadowTint="secondary"
+				aria-label={$t('home.footer.socials.linkedin')}
+				href={me.links.linkedin}
+			>
+				<LogoLinkedIn />
+			</AtomButton>
+		</li>
+		<li>
+			<AtomButton
+				variant="text"
+				color="inverted"
+				shadow
+				shadowTint="secondary"
+				aria-label={$t('home.footer.socials.github')}
+				href={me.links.github}
+			>
+				<LogoGithub />
+			</AtomButton>
+		</li>
+	</ul>
+	<span class="font-stylized">
+		{$t('home.footer.author')}
+	</span>
+	<span class="font-stylized text-xs">
+		{$t('home.footer.frameworks')}
+	</span>
+</footer>
